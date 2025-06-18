f4fbd48a8a82bd206fcf63738d511d68531abbdb
Victor <victorclovisb@hotmail.com>
2025-06-18 16:25:33 -0300

    feat: enhance EnvConfig module and service with ConfigService integration

--- src/shared/infrastructure/env-config/env-config.module.ts
+++ src/shared/infrastructure/env-config/env-config.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { EnvConfigService } from './env-config.service';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
-  providers: [EnvConfigService],
+  providers: [EnvConfigService, ConfigService],
 })
 export class EnvConfigModule {}
