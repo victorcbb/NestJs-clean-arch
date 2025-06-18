3854c2a84ab95b55086eda2e7ef2711d2e55079c
Victor <victorclovisb@hotmail.com>
2025-06-18 16:50:31 -0300

    feat: add forRoot method to EnvConfigModule for dynamic configuration

--- src/shared/infrastructure/env-config/env-config.module.ts
+++ src/shared/infrastructure/env-config/env-config.module.ts
@@ -1,8 +1,30 @@
-import { Module } from '@nestjs/common';
+import { DynamicModule, Module } from '@nestjs/common';
 import { EnvConfigService } from './env-config.service';
-import { ConfigService } from '@nestjs/config';
+import {
+  ConfigModule,
+  ConfigModuleOptions,
+  ConfigService,
+} from '@nestjs/config';
+import { join } from 'node:path';
 
 @Module({
   providers: [EnvConfigService, ConfigService],
 })
-export class EnvConfigModule {}
+export class EnvConfigModule {
+  static forRoot(options: ConfigModuleOptions = {}): DynamicModule {
+    return {
+      module: EnvConfigModule,
+      imports: [
+        ConfigModule.forRoot({
+          ...options,
+          envFilePath: [
+            join(
+              __dirname,
+              `../../../../.env.${process.env.NODE_ENV || 'development'}`,
+            ),
+          ],
+        }),
+      ],
+    };
+  }
+}
