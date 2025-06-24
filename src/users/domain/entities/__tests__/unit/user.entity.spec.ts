4c5774a6c808a4e88d4a3a468c2be6a4320f98d7
Victor <victorclovisb@hotmail.com>
2025-06-24 20:09:00 -0300

    refactor: replace getter methods with property access in UserEntity tests

--- src/users/domain/entities/__tests__/unit/user.entity.spec.ts
+++ src/users/domain/entities/__tests__/unit/user.entity.spec.ts
@@ -12,33 +12,33 @@ describe('UserEntity unit test', () => {
   })
 
   it('Constructor method', () => {
-    expect(sut.getName()).toBe(props.name)
-    expect(sut.getEmail()).toBe(props.email)
-    expect(sut.getPassword()).toBe(props.password)
-    expect(sut.getCreatedAt()).toBeInstanceOf(Date)
+    expect(sut.name).toBe(props.name)
+    expect(sut.email).toBe(props.email)
+    expect(sut.password).toBe(props.password)
+    expect(sut.createdAt).toBeInstanceOf(Date)
   })
 
   it('Getter of name field', () => {
-    expect(sut.getName()).toBeDefined()
-    expect(sut.getName()).toBe(props.name)
-    expect(typeof sut.getName()).toBe('string')
+    expect(sut.name).toBeDefined()
+    expect(sut.name).toBe(props.name)
+    expect(typeof sut.name).toBe('string')
   })
 
   it('Getter of name field', () => {
-    expect(sut.getEmail()).toBeDefined()
-    expect(sut.getEmail()).toBe(props.email)
-    expect(typeof sut.getEmail()).toBe('string')
+    expect(sut.email).toBeDefined()
+    expect(sut.email).toBe(props.email)
+    expect(typeof sut.email).toBe('string')
   })
 
   it('Getter of password field', () => {
     // O ideal seria ter o password criptografado e ter um método para verificar se a senha informada é igual a senha do usuário
-    expect(sut.getPassword()).toBeDefined()
-    expect(sut.getPassword()).toBe(props.password)
-    expect(typeof sut.getPassword()).toBe('string')
+    expect(sut.password).toBeDefined()
+    expect(sut.password).toBe(props.password)
+    expect(typeof sut.password).toBe('string')
   })
 
   it('Getter of createdAt field', () => {
-    expect(sut.getCreatedAt()).toBeDefined()
-    expect(sut.getCreatedAt()).toBeInstanceOf(Date)
+    expect(sut.createdAt).toBeDefined()
+    expect(sut.createdAt).toBeInstanceOf(Date)
   })
 })
