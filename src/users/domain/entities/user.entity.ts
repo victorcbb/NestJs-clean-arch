6592d5db0495b89dcb540d677433c0a5cdbee558
Victor <victorclovisb@hotmail.com>
2025-06-25 09:25:32 -0300

    refactor: add private setters for name, email, and password in UserEntity

--- src/users/domain/entities/user.entity.ts
+++ src/users/domain/entities/user.entity.ts
@@ -20,15 +20,35 @@ export class UserEntity extends Entity<UserProps> {
     return this.props.name
   }
 
+  private set name(value: string) {
+    this.props.name = value
+  }
+
   get email() {
     return this.props.email
+  }
+
+  private set email(value: string) {
+    this.props.email = value
   }
 
   get password() {
     return this.props.password
   }
 
+  private set password(value: string) {
+    this.props.password = value
+  }
+
   get createdAt() {
     return this.props.createdAt
+  }
+
+  updateName(value: string): void {
+    this.name = value
+  }
+
+  updatePassword(value: string): void {
+    this.password = value
   }
 }
