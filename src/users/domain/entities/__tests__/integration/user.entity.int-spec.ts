19a0e74777a9224fd8a6afa4bd5b159105ef6633
Victor <victorclovisb@hotmail.com>
2025-06-30 11:35:02 -0300

    test: add integration test for valid UserEntity creation

--- src/users/domain/entities/__tests__/integration/user.entity.int-spec.ts
+++ src/users/domain/entities/__tests__/integration/user.entity.int-spec.ts
@@ -102,5 +102,14 @@ describe('UserEntity integration tests', () => {
 
       expect(() => new UserEntity(props)).toThrow(EntityValidationError)
     })
+
+    it('Should a valid user', () => {
+      expect.assertions(0)
+      const props: UserProps = {
+        ...UserDataBuilder({}),
+      }
+
+      new UserEntity(props)
+    })
   })
 })
