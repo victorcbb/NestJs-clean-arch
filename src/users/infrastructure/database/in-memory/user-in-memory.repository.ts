76f9e6b2ecc574ef92a6a00f53854ae39f4a9227
Victor <victorclovisb@hotmail.com>
2025-07-14 14:22:51 -0300

    refactor: replace InMemoryRepository with InMemorySearchableRepository in UserInMemoryRepository

--- src/users/infrastructure/database/in-memory/user-in-memory.repository.ts
+++ src/users/infrastructure/database/in-memory/user-in-memory.repository.ts
@@ -1,9 +1,9 @@
-import { InMemoryRepository } from '@/shared/domain/repositories/in-memory.repository'
+import { InMemorySearchableRepository } from '@/shared/domain/repositories/in-memory-searchable.repository'
 import { UserEntity } from '@/users/domain/entities/user.entity'
 import { UserRepositoory } from '@/users/domain/repositories/user.repository'
 
 export class UserInMemoryRepository
-  extends InMemoryRepository<UserEntity>
+  extends InMemorySearchableRepository<UserEntity>
   implements UserRepositoory
 {
   async findByEmail(email: string): Promise<UserEntity | null> {
