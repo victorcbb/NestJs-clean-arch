1ed5b267099a6bc2dde7473e242c44ffa37f1b1c
Victor <victorclovisb@hotmail.com>
2025-07-14 14:25:56 -0300

    feat: implement InMemorySearchableRepository for in-memory entity search functionality

---
+++ src/shared/domain/repositories/in-memory-searchable.repository.ts
@@ -0,0 +1,14 @@
+import { Entity } from '../entities/entity'
+import { InMemoryRepository } from './in-memory.repository'
+import { SearchableRepositoryInterface } from './searchable-repository-contract'
+
+export abstract class InMemorySearchableRepository<E extends Entity>
+  extends InMemoryRepository<E>
+  implements SearchableRepositoryInterface<E, any, any>
+{
+  item: E[] = []
+
+  search(props: any): Promise<any> {
+    throw new Error('Method not implemented.')
+  }
+}
