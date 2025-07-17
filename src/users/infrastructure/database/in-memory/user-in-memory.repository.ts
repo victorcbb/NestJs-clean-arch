89ad224bf23ea5a8e8dcf28b31da5b844ca50919
Victor <victorclovisb@hotmail.com>
2025-07-17 09:39:33 -0300

    refactor: implement filtering and sorting methods in UserInMemoryRepository for enhanced data retrieval capabilities

--- src/users/infrastructure/database/in-memory/user-in-memory.repository.ts
+++ src/users/infrastructure/database/in-memory/user-in-memory.repository.ts
@@ -1,15 +1,61 @@
 import { InMemorySearchableRepository } from '@/shared/domain/repositories/in-memory-searchable.repository'
+import { SortDirection } from '@/shared/domain/repositories/searchable-repository-contract'
 import { UserEntity } from '@/users/domain/entities/user.entity'
-import { UserRepositoory } from '@/users/domain/repositories/user.repository'
+import {
+  UserFilter,
+  UserRepositoory,
+} from '@/users/domain/repositories/user.repository'
 
 export class UserInMemoryRepository
   extends InMemorySearchableRepository<UserEntity>
   implements UserRepositoory
 {
+  declare sortableFields: ['name', 'createdAt']
+
   async findByEmail(email: string): Promise<UserEntity | null> {
-    return this.item.find(user => user.email === email)
+    return this.items.find(user => user.email === email)
   }
   async emailExists(email: string): Promise<boolean> {
-    return this.item.some(user => user.email === email)
+    return this.items.some(user => user.email === email)
+  }
+  protected async applyFilter(
+    items: UserEntity[],
+    filter: UserFilter,
+  ): Promise<UserEntity[]> {
+    if (!filter) {
+      return items
+    }
+
+    if (filter.name && filter.email) {
+      return items.filter(
+        item =>
+          item.name.toLowerCase().includes(filter.name.toLowerCase()) &&
+          item.email.toLowerCase().includes(filter.email.toLowerCase()),
+      )
+    }
+
+    if (filter.name) {
+      return items.filter(item =>
+        item.name.toLowerCase().includes(filter.name.toLowerCase()),
+      )
+    }
+
+    if (filter.email) {
+      return items.filter(item =>
+        item.email.toLowerCase().includes(filter.email.toLowerCase()),
+      )
+    }
+
+    return items
+  }
+
+  protected async applySort(
+    items: UserEntity[],
+    sort: string | null,
+    sortDirection: SortDirection,
+  ): Promise<UserEntity[]> {
+    return !sort
+      ? super.applySort(items, 'createdAt', 'desc')
+      : super.applySort(items, sort, sortDirection)
   }
 }
