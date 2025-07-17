073f9eb874c6675f63760037f6e2140d934e836e
Victor <victorclovisb@hotmail.com>
2025-07-17 09:14:35 -0300

    refactor: enhance UserRepository interface to utilize specific SearchParams and SearchResult types for improved type safety and clarity

--- src/users/domain/repositories/user.repository.ts
+++ src/users/domain/repositories/user.repository.ts
@@ -1,8 +1,22 @@
 import { UserEntity } from '../entities/user.entity'
-import { SearchableRepositoryInterface } from '@/shared/domain/repositories/searchable-repository-contract'
+import {
+  SearchParams as DefaultSearchParams,
+  SearchResult as DefaultSearchResult,
+  SearchableRepositoryInterface,
+} from '@/shared/domain/repositories/searchable-repository-contract'
+
+export type UserFilter = { name: string; email: string }
+
+export class SearchParams extends DefaultSearchParams<UserFilter> {}
+export class SearchResult extends DefaultSearchResult<UserEntity, UserFilter> {}
 
 export interface UserRepositoory
-  extends SearchableRepositoryInterface<UserEntity, any, any> {
+  extends SearchableRepositoryInterface<
+    UserEntity,
+    UserFilter,
+    SearchParams,
+    SearchResult
+  > {
   findByEmail(email: string): Promise<UserEntity | null>
   emailExists(email: string): Promise<boolean>
 }
