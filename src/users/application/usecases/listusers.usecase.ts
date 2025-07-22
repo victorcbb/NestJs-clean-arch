e5b93049c03f2ad9cec1baff4004452f90bb1f3a
Victor <victorclovisb@hotmail.com>
2025-07-22 09:56:05 -0300

    feat: implement ListUserUseCase for retrieving and paginating user search results

---
+++ src/users/application/usecases/listusers.usecase.ts
@@ -0,0 +1,41 @@
+import {
+  SearchParams,
+  SearchResult,
+  UserRepositoory,
+} from '@/users/domain/repositories/user.repository'
+import { UseCase } from '@/shared/application/usecases/use-case'
+import { SearchInput } from '@/shared/application/dtos/search-input'
+import { UserOutput, UserOutputMapper } from '../dto/user-output'
+import {
+  PaginationOutput,
+  PaginationOutputMapper,
+} from '@/shared/application/dtos/pagination-output'
+
+export type ListUsersInput = SearchInput<{
+  name?: string
+  email?: string
+}>
+
+export type ListUsersOutput = PaginationOutput<UserOutput>
+
+export class ListUserUseCase
+  implements UseCase<ListUsersInput, ListUsersOutput>
+{
+  constructor(private userRepository: UserRepositoory) {}
+
+  async execute(input: ListUsersInput): Promise<ListUsersOutput> {
+    const params = new SearchParams(input)
+
+    const searchResult = await this.userRepository.search(params)
+
+    return this.toOutput(searchResult)
+  }
+
+  private toOutput(searchResult: SearchResult): ListUsersOutput {
+    const items = searchResult.items.map(item => {
+      return UserOutputMapper.toOutput(item)
+    })
+
+    return PaginationOutputMapper.toOutput(items, searchResult)
+  }
+}
